9163ab93383d136f33c48f94de3babbebc15bbf2
PongNP <pong.nopphorn@gmail.com>
2020-06-02 16:06:10 +0700

    Fixed Google Map Api key

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -21,7 +21,7 @@
     <body>
         <div id="map"></div>
         <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBYfGJkrRVvNkhoVkgdIew_3PKuq6ecMVk&callback=initMap" async defer></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key={{ env('APIKEY_GOOGLEMAP') }}&callback=initMap" async defer></script>
         <script>
             var map;
             var infoWindow;
